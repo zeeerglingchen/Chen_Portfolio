a7d6d89331f2ef3c8601e79e6c89f02746b2b188
ChenyangLyu <46425861+zeeerglingchen@users.noreply.github.com>
2019-04-19 15:24:30 -0400

    Add files via upload

---
+++ js/typewriter.js
@@ -0,0 +1,18 @@
+var txt = "Hope you enjoyed my work !"
+var container = document.querySelector("#writer p")
+var count = 0;
+var speed = 100;
+
+function typewriter() {
+  if (count < txt.length) {
+    document.getElementById("greeting").innerHTML += txt.charAt(count);
+    count++;
+    setTimeout(typewriter, speed);
+  } else{
+    document.getElementById("greeting").innerHTML = "";
+    count = 0;
+    typewriter()
+  }
+}
+
+window.onload = typewriter()
